9c16129f62728c77511329f4f05bad03b4c8568f
rima820801-sudo <rima820801@gmail.com>
2025-12-01 17:51:04 -0600

    Preparando frontend para despliegue web

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-    plugins: [react(), viteSingleFile()],
-    base: './',
+    plugins: [react()],
     build: {
-        outDir: 'dist_usb',
-        assetsInlineLimit: 100000000,
-        cssCodeSplit: false,
-        rollupOptions: {
-            output: {
-                inlineDynamicImports: true,
-            },
-        },
+        outDir: 'dist',
     },
     server: {
         port: 3000,
